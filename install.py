9700083193fdf40d04b93817f1adeebe5288169c
codemobiles <chaiyasit.t@gmail.com>
2021-03-16 14:50:06 +0700

    1234

---
+++ install.py
@@ -0,0 +1,11 @@
+# windows
+https://www.python.org/downloads/
+
+
+# mac
+brew --version
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install python
+
+# Upgrade python to version 3.9
+brew upgrade python@3.9
