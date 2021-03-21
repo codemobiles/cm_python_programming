d0e646b84347038629d5ef864d4b134f9b557e28
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 13:39:51 +0700

    1234

---
+++ workshops/demo2.py
@@ -0,0 +1,8 @@
+print("Hello VSCode 4")
+print("Hello VSCode 5")
+print("Hello VSCode 6")
+
+if 5 > 2 :
+ print("5 > 2")
+
+print("5 > 2")
---
+++ workshops/demo3.py
@@ -0,0 +1,10 @@
+print("Helloworld")
+print("Helloworld")
+
+if 3 > 1 and True:
+    print("3 > 1")
+    print("3 > 1")
+    print("3 > 1")
+else:
+    print("3 <= 1")
+    print("3 <= 1")
