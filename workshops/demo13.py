6b08f07c40d178faf4f8ec3e99ca42fc0912228e
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 21:13:31 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep18_Functions

--- workshops/demo13.py
+++ workshops/demo13.py
@@ -1,17 +1,24 @@
 def test1():
-    print("test1")
-    print("test1")
-    print("test1")
-
-def test1():
-    print("test1")
-    print("test1")
-    print("test1")
+    print("call test1")
 
 
-def test2(x: str, y: int, z: int):
-    print(x+str(y)+str(z))
+def test2(x, y, z):
+    print(x, y, z)
+
+
+def test3(x: int, y: str, z: bool):
+    print(str(x), str(y), str(z))
+
+
+def test4(a: int, b: int) -> float:
+    return float(a+b), "ok"
 
 
 test1()
-test2(x="lek", y=1, z=3)
+test2(1, 2, 3)
+test2(x=1, y=2, z=3)
+test3(x=3, y="ok", z=True)
+print(test4(2, 3))
+
+data, result = test4(1, 5)
+print(data, result)
