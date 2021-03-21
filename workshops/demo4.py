cf9673a41c92beed2d72e046bdf7be8d95473f06
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 14:47:30 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep9_Variable_Declaration

---
+++ workshops/demo4.py
@@ -0,0 +1,29 @@
+# Implicit Declaration
+tmp1 = 1  # integer
+tmp2 = 2  # integer
+tmp3 = "Hey"  # string
+tmp4 = 'Hey'  # string
+tmp5 = 1.1  # float
+tmp6 = False  # bool
+tmp66 = {"username": "admin", "password": "1234"}
+tmp77 = ["Angular", "React", "Vue"]
+print(tmp66)
+print(tmp77)
+
+print(tmp1)
+print(tmp2)
+print(tmp3)
+print(tmp4)
+
+# Explicit Declaration
+tmp7 = int(7)
+tmp8 = float(7.2)
+tmp9 = str(7.2)
+tmp10 = bool(True)
+tmp11 = dict(username="admin", password="1234")
+tmp12 = list(("Angular", "React", "Vue"))
+
+print(type(tmp8))
+print(type(tmp9))
+print(tmp11)
+print(tmp12)
