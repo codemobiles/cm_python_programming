46f313049da8023ed9e507b2f459cbe84e459fc6
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 16:20:44 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep16_String_Methods

---
+++ workshops/demo11.py
@@ -0,0 +1,14 @@
+tmp = "CodeMobiles"
+
+print(tmp)
+print(tmp.lower())
+print(tmp.upper())
+print(tmp.upper())
+print(tmp.capitalize())
+
+# alphanumeric 0-9,a-Z,.,_
+tmp1 = "1234h.@ey"
+print(tmp1.isalnum())
+
+tmp2 = "I am lek codemobiles"
+print(tmp2.find("lek"))
