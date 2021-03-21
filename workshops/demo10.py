2f1f4f91e605a3f168f1be80c24de450d54ef81d
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 16:08:30 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep15_String_Escape_Charactor

---
+++ workshops/demo10.py
@@ -0,0 +1,6 @@
+tmp = "CodeMobiles\'s the \"best\" traning center\nDo you believe?"
+print(tmp)
+
+# A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
