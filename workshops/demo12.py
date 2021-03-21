28ce6d944f4ec510dbf130f8aa17264650f9f6ea
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 16:41:53 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep17_Boolean_Operator

---
+++ workshops/demo12.py
@@ -0,0 +1,18 @@
+print(True)
+
+tmp1 = len("lek") == 4
+tmp1 = len("lek") >= 2
+tmp1 = True
+print(not tmp1)
+
+tmp2 = bool(0)
+print(type(tmp2))
+print(tmp2)
+
+print(1.0 / 7.0)
+print(3 % 3)
+
+tmp3 = 1
+tmp3 += 20
+tmp3 += 1
+print(tmp3)
