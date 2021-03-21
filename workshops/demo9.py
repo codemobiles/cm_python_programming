e216f72849c13c705ace40edb328cd70cc3e7941
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 15:40:32 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep14_String_Compare_Concat_Format

---
+++ workshops/demo9.py
@@ -0,0 +1,20 @@
+# String
+tmp1 = "CodeMobiles"
+tmp1 = 'CodeMobiles'
+lenOfTmp1 = len(tmp1)
+
+print(lenOfTmp1)
+
+if tmp1 == 'CodeMobiles':
+    print(tmp1 + " is equal " + "CodeMobiles")
+else:
+    print(tmp1)
+
+
+x, y, z = 1, 2, 3
+print("x is {},y is {},z is {}".format(x, y, z))
+
+if "hey" in "hey, what's up":
+    print("there is hey")
+
+    
