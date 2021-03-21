0c6b860ed14cf15953f2dd66395c3657a214e7bd
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 15:12:59 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep11_Variable_Multiple_Assignment

---
+++ workshops/demo5.py
@@ -0,0 +1,14 @@
+# camel case
+dataArray = [1, 2, 3]
+dataOfArray1 = [1, 2, 3]
+
+# Pascal case
+DataArray = [3, 2, 1]
+DataOfArray1 = [1, 2, 3]
+
+# snake case
+data_array = [3, 2, 1]
+data_of_array1 = [3, 2, 1]
+
+print("dataArray : " + str(dataArray))
+print("DataArray : " + str(DataArray))
---
+++ workshops/demo6.py
@@ -0,0 +1,13 @@
+def getData():
+    return 0, 1, 2
+
+
+x = y = z = 0
+x1, y1, z1 = getData()
+
+courses = ["Angular", "Python", "React"]
+angularTitle, pythonTitle, reactTitle = courses
+
+print(x, y, z)
+print(x1, y1, z1)
+print(angularTitle, pythonTitle, reactTitle)
