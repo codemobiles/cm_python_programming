7156556f8da4e2cfaa25cf1182ccde8ba134237e
codemobiles <chaiyasit.t@gmail.com>
2021-03-21 13:57:37 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep8_Comment

--- workshops/demo3.py
+++ workshops/demo3.py
@@ -1,6 +1,13 @@
-print("Helloworld")
-print("Helloworld")
+# print("Helloworld") 
+# print("Helloworld")
 
+"""
+555
+555
+55
+"""
+
+# Demonstrate indentation
 if 3 > 1 and True:
     print("3 > 1")
     print("3 > 1")
