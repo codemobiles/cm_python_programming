e7e8da2e4624b49711cfc113a77a7ffde1c13a06
codemobiles <chaiyasit.t@gmail.com>
2021-03-22 19:54:44 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep20_If_Else

---
+++ workshops/demo15.py
@@ -0,0 +1,17 @@
+# condition if-else
+
+data = 1
+if data > 3 and data < 5:
+    print("data {}".format(data))
+    print("data {}".format(data))
+    print("data {}".format(data))
+elif data > 4:
+    print("data > 4 : {}".format(data))
+else:
+    print("else data is not > 3")
+
+
+if True:
+    print("a is greater than b")
+
+print("Yes") if False else print("No")
