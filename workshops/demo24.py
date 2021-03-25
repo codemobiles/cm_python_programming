6756e181800779041042c3e213e87a47b53affd9
codemobiles <chaiyasit.t@gmail.com>
2021-03-25 07:49:44 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep29_pass_to_be_done

---
+++ workshops/demo24.py
@@ -0,0 +1,28 @@
+# Define Class
+class User:
+    username = ""
+    password = ""
+
+    def __init__(self, username="admin", password="1234"):
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "username: {}, password: {}".format(self.username, self.password)
+
+    def print(self):
+        print("created : {}, {}".format(self.username, self.password))
+
+    def clear():
+        pass
+
+
+# Create an object from above class
+user1 = User("lek", "555")
+# user1.print()
+print(user1)
+
+
+class Payment:
+    pass
+
