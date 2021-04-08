70dceedbb12e56f0440bd6beee0f96f805638fbf
codemobiles <chaiyasit.t@gmail.com>
2021-04-08 10:19:55 +0700

    เรียนเขียน Python พร้อม Sourcecode Ep41_Tuple

---
+++ workshops/demo37.py
@@ -0,0 +1,14 @@
+# Tuple, List, Set, Dictionary
+colors = ["Red", "Green", "Blue"]
+print(colors)
+colors[0] = "Yellow"
+print(colors)
+
+courses = ("Angular", "VueJS", "React", "Python", "Python")
+print(courses)
+print(courses[0])
+print(len(courses))
+# courses[0] = "NodeJS"
+
+coursesv2 = tuple(("Angular", "VueJS", "React", "Python", "Python"))
+print(coursesv2)
